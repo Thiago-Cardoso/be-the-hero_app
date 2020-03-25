a5f121c6d7546cd7fcc8bf742fd12be0e3f70440
Thiago-Cardoso <thiago.cardosoo@hotmail.com>
2020-03-25 00:34:33 -0300

    api

---
+++ backend/src/controllers/ProfileController.js
@@ -0,0 +1,10 @@
+module.exports = {
+   
+    async index(request, response){
+        const ong_id = request.headers.authorization;
+
+        const incidents = await connection('incidents')
+            .where('ong_id', ong_id)
+            .select('*')
+    }
+}
